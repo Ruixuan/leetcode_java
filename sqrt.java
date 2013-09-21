c9335300814b6388b7a9cc82a16119a02454a9bd
Ruixuan <hou@kinsman.cs.dartmouth.edu>
2013-09-21 16:32:08 -0400

    sqrt ac

---
+++ sqrt.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int sqrt(int x) {
+        // tricky part
+        //
+        if ( x == 0){
+            return 0;
+        }
+        int head, tail, k;       
+        int tmp;
+        head = 1;
+        tail = (x ) / 2 + 1;
+        while(head <= tail){
+            // tricky part 1: avoid int over flow
+            k = head + ( - head + tail) / 2;
+            // tricky part 2: using div rather than multiply
+            tmp = x / k;
+            if ( tmp == k){
+                return k;
+            }
+            if ( k < tmp ){
+                head = k + 1;
+            }else{
+                tail = k - 1;
+            }
+        }
+        return tail;
+    }
+}
