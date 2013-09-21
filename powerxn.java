8011b9e14b6dd00ef1272cf6fd46676d68d276e9
Ruixuan <hou@tahoe.cs.dartmouth.edu>
2013-09-21 10:06:17 -0400

    powexn

---
+++ powerxn.java
@@ -0,0 +1,29 @@
+public class Solution {
+        public double pow(double x, int n) {
+            // power(x, n ) = power(x*x, n/2) * x^(n mod 2)
+            int i,k;
+            int flag = 0;
+            if ( x ==0){
+                return 0;
+            }
+            if ( n < 0){
+                n = -n;
+                flag = 1;
+            }
+            double answer;
+            answer = 1;
+            while( n > 0){
+                k = n % 2;
+                if (k == 1){
+                    answer *= x;
+                }// other wise answer = answer * 1
+                x = x * x;
+                n = n / 2;
+            }
+            if (flag){
+                return 1/answer;
+            }else{
+                return answer;
+            }
+    }
+}
