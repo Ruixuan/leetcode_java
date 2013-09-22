59d1ca7b74e1a5ecf22abb6b6437db865a7052cf
Ruixuan <ruixuanhou@gmail.com>
2013-09-22 15:01:33 -0400

    Create isSymmetricTree.java

---
+++ isSymmetricTree.java
@@ -0,0 +1,16 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        
+    }
+}
