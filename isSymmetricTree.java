11ded72da70b05daf06cac61c22f937d41cec5a8
Ruixuan <hou@tahoe.cs.dartmouth.edu>
2013-09-22 15:10:19 -0400

    is sym done

--- isSymmetricTree.java
+++ isSymmetricTree.java
@@ -8,9 +8,18 @@
  * }
  */
 public class Solution {
+    private boolean compare(TreeNode root_l,TreeNode root_r){
+        if (root_l == null || root_r == null)
+            return root_l == null && root_r == null;
+        if ( root_l.val != root_r.val)
+            return false;
+        if (! compare( root_l.left, root_r.right))
+            return false;
+        return compare( root_l.right, root_r.left);
+    }
     public boolean isSymmetric(TreeNode root) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        
+        return compare(root, root);       
     }
 }
