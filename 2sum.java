cd8fac20c403de62adb8576021c0bb9cc75b927b
Ruixuan <ruixuanhou@gmail.com>
2013-09-23 12:40:12 -0400

    Create 2sum.java

---
+++ 2sum.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        
+    }
+}
