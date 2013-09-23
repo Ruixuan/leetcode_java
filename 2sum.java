5d98d2c67a06a7e9314e86cf857bce2e99359393
Ruixuan <ruixuanhou@gmail.com>
2013-09-23 12:54:02 -0400

    Update 2sum.java

--- 2sum.java
+++ 2sum.java
@@ -2,6 +2,22 @@ public class Solution {
     public int[] twoSum(int[] numbers, int target) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        
+        int head, tail;
+        head = 0;
+        tail = numbers.length - 1;
+        while( head < tail ){
+            while(numbers[head] + numbers[tail] > target){
+                head ++;
+            }
+            while(numbers[head] + numbers[tail] > target ){
+                tail ++;
+            }
+            if ( numbers[head] + numbers[tail] == target){
+                int[] answer = new int[2];
+                answer[0] = head;
+                answer[1] = tail;
+                return answer;
+            }
+        }
     }
 }
