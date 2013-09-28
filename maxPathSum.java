153defa214493ed7fdef08c824001f9a3d6fff66
Ruixuan <ruixuanhou@gmail.com>
2013-09-28 11:07:05 -0400

    Update maxPathSum.java

--- maxPathSum.java
+++ maxPathSum.java
@@ -8,10 +8,29 @@
  * }
  */
 public class Solution {
+    private int max;
+    private int max_val(TreeNode root){
+        if (null == root)
+            return 0;
+        int val_l, val_r;
+        val_l = max_val(root.left);
+        val_r = max_val(root.right);
+        if (val_l +　val_r + root.val > max)
+            max = val_l +　val_r + root.val;
+        int ret_val;
+        ret_val = root.val + val_l;
+        if (ret_val < root.val + val_r)
+            ret_val = root.val + val_r;
+        if (ret_val < 0)
+            ret_val = 0;
+        return ret_val;
+    }
     public int maxPathSum(TreeNode root) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        
+        max = 0;
+        max_val(root);
+        return max;
         
     }
 }
