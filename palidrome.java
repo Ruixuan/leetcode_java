d70f1af39ed89acb25616315cf90fb00f3cdaac1
Ruixuan <ruixuanhou@gmail.com>
2013-09-28 10:56:46 -0400

    Create palidrome.java

---
+++ palidrome.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public boolean isPalindrome(String s) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        s = s.toLowerCase();
+        
+        
+    }
+}
