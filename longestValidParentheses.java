863ac30f4def6bfab013839d622bacf8e2cab43f
Ruixuan <hou@tahoe.cs.dartmouth.edu>
2013-09-27 23:47:10 -0400

    finish longestparenttheses

--- longestValidParentheses.java
+++ longestValidParentheses.java
@@ -2,6 +2,52 @@ public class Solution {
     public int longestValidParentheses(String s) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        
+        char[] tokens = s.toCharArray();
+        int n = tokens.length();
+        int[] left_match = new int[n]; // ')'
+        int[] righ_match = new int[n]; // '('
+        int left_count;
+        int right_count;
+        left_count = 0;
+        for( int i = 0; i < n ; i ++){
+            if (tokens[i] == '(')
+                left_count ++;
+            else
+                left_count --;
+            
+            if ( left_count >= 0)
+                left_match[i] = 1;
+            else{
+                left_match[i] = 0;
+                //reset, start over
+                left_count = 0;
+            }
+       }
+       right_count = 0;
+       for(int i = n - 1; i >= 0; i--){
+            if (tokens[i] == ')')
+                right_count ++;
+            else
+                right_count --;
+            if ( reft_count >= 0)
+                righ_match[i] = 1;
+            else{
+                righ_match[i] = 0;
+                // reset 
+                righ_count = 0;
+            }
+       }
+       int curr_len = 0 ; 
+       int longest = 0;
+       for(int i = 0; i < n ; i ++)
+           if ( righ_match[i] + left_match[i] == 2){
+               curr_len ++;
+               if (curr_len > longest)
+                   longest = curr_len;
+           }else
+               curr_len = 0;
+
+       return longest;
+
     }
 }
