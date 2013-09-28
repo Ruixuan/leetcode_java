366bc9caf4c64239c67f98be3fe8126d448d9ba2
Ruixuan <ruixuanhou@gmail.com>
2013-09-28 11:01:24 -0400

    Create maxPathSum.java

---
+++ maxPathSum.java
@@ -0,0 +1,17 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int maxPathSum(TreeNode root) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        
+        
+    }
+}
