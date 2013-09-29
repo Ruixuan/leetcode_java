23452f375ff3986703b2cf781b2d1e97deb969f5
Ruixuan <ruixuanhou@gmail.com>
2013-09-29 00:45:43 -0400

    solve over flow problem

--- codility/equilibrium.java
+++ codility/equilibrium.java
@@ -2,14 +2,14 @@ class Solution {
     public int solution(int[] A) {
         // write your code here...
         int n = A.length;
-        int[] sum = new int[n];
+        long[] sum = new int[n];
         if (n > 0)
             sum[0] = A[0];
         else
-            return 0;
+            return -1;
         for(int i = 1; i < n; i ++)
             sum[i] = sum[i -1] + A[i];
-        int total = sum[n - 1];
+        long total = sum[n - 1];
         int answer = 0;
         for(int i = 0; i < n ; i ++)
             if ( total - sum[i] == sum[i] - A[i])
