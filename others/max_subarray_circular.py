aa3db6a6b9b4d2c42230c584b722949086095147
Ruixuan <hou@wildcat.cs.dartmouth.edu>
2013-10-09 18:44:50 -0400

    max circular subarray

--- others/max_subarray_circular.py
+++ others/max_subarray_circular.py
@@ -1,13 +1,25 @@
 def max_circular_subarray(A):
     max_end = 0
+    max_len = 0
     max_so_far = 0
-    for x in A:
-        max_end = max(0, max_end + x)
-        max_so_far = max( max_end, max_so_far)
+    AA = A + A
+    print AA
+    n = len(A)
+    for i in range(len(AA)):
+        if max_len == n:
+            max_len -= 1
+            max_end -= AA[i - n]
+        if max_end + AA[i] > 0:
+            max_len += 1
+            max_end = max_end + AA[i]
+        else:
+            max_len = 0
+            max_end = 0
+        max_so_far = max(max_end, max_so_far)
     return max_so_far
         
 # test
-A = [ 2, -3, 5, 8, 0 ]
+A = [ 2, -3, 5, -9, 3 ]
 print A
 print max_circular_subarray(A)
 
