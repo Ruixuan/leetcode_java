273f39c317e3b1f59aa1f16b62d64227651de8b7
Ruixuan <ruixuanhou@gmail.com>
2013-10-10 20:06:00 -0400

    Create randomlinkedlistcopy.java

---
+++ randomlinkedlistcopy.java
@@ -0,0 +1,14 @@
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        
+    }
+}
