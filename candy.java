5f1e94d6595307fef65053129b466f28aceff6f4
Ruixuan <ruixuanhou@gmail.com>
2013-10-10 22:36:35 -0400

    pass test done!

--- candy.java
+++ candy.java
@@ -1,21 +1,55 @@
 public class Solution{
     public int candy(int[] ratings){
-        int n = ratings.length;
-        int[] candies = new int[n];
-        candies[0] = 1;
-        int min = 1;
-        int sum = 1;
-        for (int i = 1; i < n ; i ++){
-            candies[i] = candies[i - 1];
-            if (ratings[i] > ratings[i-1])
-                candies[i] = candies[i-1] + 1;
-            if (ratings[i] < ratings[i-1])
-                candies[i] = candies[i-1] - 1;
-            if (min > candies[i])
-            a    min = candies[i];
-            sum += candies[i];
-        }
-        sum -= n * min;
-        return sum;
+		// think it as a topological sort problem
+		// because there are some dependencies
+		// a < b , then there is a dependency a -> b
+		int n = ratings.length;
+		int[] pre_count = new int[n];
+		for(int i = 0; i < n; i ++)
+			pre_count[i] = 0;
+		
+		for(int i = 1; i < n; i ++){
+			if (ratings[i] < ratings[i-1])
+				pre_count[i-1] ++;
+			if (ratings[i] > ratings[i-1])
+				pre_count[i]++;				
+		}
+		int[] candies = new int[n];
+		LinkedList<Integer> topo_que = new LinkedList<Integer>();
+		for(int i = 0; i < n; i++)
+			if (pre_count[i] == 0){
+				candies[i] = 1;
+				topo_que.add(i);
+			}
+		while( ! topo_que.isEmpty()){
+			int i = topo_que.removeFirst();
+			if (i>0)
+				if ( ratings[i] < ratings[i - 1]){
+					// remove dependency
+					pre_count[i-1] --;
+					// update the candies
+					if ( candies[i] + 1 > candies[i-1])
+						candies[i-1] = candies[i] + 1;
+					if (pre_count[i-1] == 0)
+						topo_que.add(i-1);
+				}
+			if (i < n-1)
+				if ( ratings[i] < ratings[i + 1]){
+					// remove dependency
+					pre_count[i+1] --;
+					// update the candies
+					if ( candies[i] + 1 > candies[i+1])
+						candies[i+1] = candies[i] + 1;
+					if (pre_count[i+1] == 0)
+						topo_que.add(i+1);
+				}	
+		}
+		int sum = 0;
+		for(int i = 0; i < n ; i ++)
+			sum += candies[i];
+		
+		return sum;
+	
+
     }
 }
