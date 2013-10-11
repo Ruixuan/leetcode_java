347acc2b1d548848406b3af42a9a0e48f2513922
Ruixuan <hou@tahoe.cs.dartmouth.edu>
2013-10-10 20:50:07 -0400

    random cpy

--- randomlinkedlistcopy.java
+++ randomlinkedlistcopy.java
@@ -9,6 +9,23 @@
 public class Solution {
     public RandomListNode copyRandomList(RandomListNode head) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
-        
+        HashMap<RandomListNode,RandomListNode> old2new = new HashMap<RandomListNode, RandomListNode>();
+        RandomListNode pt;
+        pt = head;
+        old2new.put(null, null);
+        while(pt != null){
+            RandomListNode tmp = new RandomListNode(pt.label);
+            old2new.put(pt, tmp);
+            pt = pt.next;
+        }
+        pt = head;
+        while(pt != null){
+            RandomListNode copy;
+            copy = old2new.get(pt);
+            copy.next = old2new.get(pt.next);
+            copy.random = old2new.get(pt.random);
+            pt = pt.next;
+        }
+        return old2new.get(head);
     }
 }
