1eb7ad0445ced7d580b72cbfa9d5476fcb9cd44f
Ruixuan <ruixuanhou@gmail.com>
2013-10-13 01:02:18 -0400

    passed, still need to understand how neg bit operation works

--- singleNumberII.java
+++ singleNumberII.java
@@ -4,20 +4,20 @@ public class Solution{
         
         long[] newA = new long[n];
         for(int i = 0; i < n ; i ++)
-            newA[i] = A[i] - Integer.MIN_VALUE;
+            newA[i] = A[i];// - Integer.MIN_VALUE;
 
         int[] bits = new int[33];
         for (int i = 0; i < 32; i ++)
             bits[i] = 0;
-        for (int val: newA)
+        for (long val: newA)
             for (int i = 0; i < 32; i++)
                 bits[i] += (val >> i) & 1;
-        int result = 0;                
-        for (int i = 0; i < 31; i ++){
+        long result = 0;                
+        for (int i = 0; i < 32; i ++){
             if (bits[i] % 3 != 0)
                 result = result | (1 << i);
         }
 
-        return result + Integer.MIN_VALUE;
+        return (int)(result);
     }
 }
