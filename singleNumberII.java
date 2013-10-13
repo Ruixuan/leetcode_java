ae2b491ea1bec1d9e6f2d4f7d54d3b03ee26379e
Ruixuan <hou@tahoe.cs.dartmouth.edu>
2013-10-13 00:46:01 -0400

    change to long range

--- singleNumberII.java
+++ singleNumberII.java
@@ -1,17 +1,23 @@
 public class Solution{
     public int singleNumber(int[] A){
+        int n = A.length;
+        
+        long[] newA = new long[n];
+        for(int i = 0; i < n ; i ++)
+            newA[i] = A[i] - Integer.MIN_VALUE;
+
         int[] bits = new int[33];
         for (int i = 0; i < 32; i ++)
             bits[i] = 0;
-        for (int val: A)
+        for (int val: newA)
             for (int i = 0; i < 32; i++)
                 bits[i] += (val >> i) & 1;
         int result = 0;                
         for (int i = 0; i < 31; i ++){
-                         
             if (bits[i] % 3 != 0)
                 result = result | (1 << i);
         }
-        return result;
+
+        return result + Integer.MIN_VALUE;
     }
 }
