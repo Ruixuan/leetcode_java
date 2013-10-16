22e7d349c9195b1011db4791e29704e64de37790
Ruixuan <ruixuanhou@gmail.com>
2013-10-15 23:41:53 -0400

    Update merge_stream.java

--- others/merge_stream.java
+++ others/merge_stream.java
@@ -1,2 +1,32 @@
+
+class Node{
+  int val;
+  IntStream st;
+}
+Heap<Node> hp;
 IntStream merge(IntStream[] stream){
+  hp = new Heap();
+  for(Intstream st:stream){
+    if !st.hasNext()
+      continue;
+    Node tmp = new Node(st.next(), st);
+    hp.add(st);
+  }
+}
+
+int next(){
+  Node tmp;
+  tmp = hp.get_max();
+  hp.remove_max();
+  int result;
+  result = tmp.val;
+  if (tmp.st.hasNext()){
+    tmp.val = st.next();
+    hp.insert(tmp);
+  }
+  return result;
+}
+
+int hasNext(){
+  return !hp.isEmpty();
 }
