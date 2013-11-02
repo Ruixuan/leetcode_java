4e57b6e15dd2cc27685cafee4edb455d8eb66123
Ruixuan <ruixuanhou@gmail.com>
2013-11-02 15:56:25 -0400

    Update editdist.java

--- editdist.java
+++ editdist.java
@@ -6,11 +6,7 @@ public class Solution{
 
     private int get_val(int[][] dp, int i, int j){
         if ( i < 0 || j < 0){
-            if ( i < 0)
-                i = 0;
-            if ( j < 0)
-                j = 0;
-            return i + j;
+            return abs(i - j);
         }
         else
             return dp[i][j];
@@ -33,10 +29,13 @@ public class Solution{
         
         for( int i = 0; i < m; i ++)
             for( int j = 0; j < n; j ++){
+                dp[i][j] = i + j + 1;
                 if ( w1[i] == w2[j])
                     dp[i][j] = get_val(dp, i - 1, j - 1);
+                else 
+                    dp[i][j] =  get_val(dp, i - 1, j - 1) + 1;  
                 dp[i][j] = min(dp[i][j], min(get_val(dp,i - 1,j) + 1, get_val(dp,i, j - 1) + 1) );
             }
-        return abs(m - n) + dp[m - 1][n - 1];    
+        return  dp[m - 1][n - 1];    
     }
 }
