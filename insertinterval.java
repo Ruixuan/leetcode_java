83159750d4ea643f9397c666a073fe769c3bcf57
Ruixuan <ruixuanhou@gmail.com>
2013-11-02 17:57:24 -0400

    Create insertinterval.java

---
+++ insertinterval.java
@@ -0,0 +1,16 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public ArrayList<Interval> insert(ArrayList<Interval> intervals, Interval newInterval) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        
+    }
+}
