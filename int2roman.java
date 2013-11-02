908e2ff1b7121e9969d834f5b9b66b9aab66ea63
Ruixuan <ruixuanhou@gmail.com>
2013-11-02 13:55:28 -0400

    bug fixed done

--- int2roman.java
+++ int2roman.java
@@ -2,13 +2,15 @@ public class Solution{
     public String intToRoman(int num){
         int[] val = new int[]{1000, 900, 500, 400,  100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] str ={"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-        int i;
+        int i = 0;
         StringBuilder sb = new StringBuilder();
         while(num > 0){
             if( val[i] > num)
                 i ++;
-            else
+            else{
                 sb.append(str[i]);
+                num -= val[i];
+            }
         }
         return sb.toString();
     }
