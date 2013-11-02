20e867b811e26f786789ce2df628f671a4ce01ad
Ruixuan <ruixuanhou@gmail.com>
2013-11-02 16:47:27 -0400

    Create 4sum.java

---
+++ 4sum.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public ArrayList<ArrayList<Integer>> fourSum(int[] num, int target) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        
+    }
+}
