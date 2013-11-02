5448e562094f99e786b4f180f720677ad5920ec9
Ruixuan <ruixuanhou@gmail.com>
2013-11-02 19:58:26 -0400

    Update isScramble.java

--- isScramble.java
+++ isScramble.java
@@ -7,17 +7,19 @@ public class Solution {
         if (s1.length() != s2.length()) return false;
         char[] arr1 = s1.toCharArray();
         char[] arr2 = s2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
         for(int i = 0; i < s1.length(); i ++)
             if (arr1[i] != arr2[i])
                 return false;
         int n = s1.length();
         for(int i = 0; i < n - 1; i ++){
-            if( isScramble(s1.subString(0, i + 1), s2.subString(0, i + 1) &&
-                isScramble(s1.subString(i+1), s2.subString(i+1)) )
+            if( isScramble(s1.substring(0, i + 1), s2.substring(0, i + 1)) &&
+                isScramble(s1.substring(i+1), s2.substring(i+1)) )
                 return true;
                     
-            if( isScramble(s1.subString(0,i+1), s2.subString(n - i - 1)) &&
-                isScramble(s1.subString(i+1), s2.subString(0, n - i - 1))
+            if( isScramble(s1.substring(0,i+1), s2.substring(n - i - 1)) &&
+                isScramble(s1.substring(i+1), s2.substring(0, n - i - 1)))
                 return true;
         }
         return false;
