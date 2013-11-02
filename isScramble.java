70978f97c68a481b271e72c401127ebe1a3fe169
Ruixuan <ruixuanhou@gmail.com>
2013-11-02 19:40:03 -0400

    Create isScramble.java

---
+++ isScramble.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public boolean isScramble(String s1, String s2) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+               
+    }
+}
