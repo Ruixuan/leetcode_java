3e56b72b3d66919fba9bca84bc18fa4085e358a9
Ruixuan <hou@wildcat.cs.dartmouth.edu>
2013-11-02 16:18:06 -0400

    phone number

---
+++ phonenumber.java
@@ -0,0 +1,6 @@
+public class Solution{
+    public ArrayList<String> letterCombinations(String digits){
+        ArrayList<String> answer = new ArrayList<String>();
+        HashMap<Character, char[]> digit_map = new HashMap<Character, char[]>();
+    }
+}
