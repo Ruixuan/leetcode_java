d1363944b775fda08e8d60afe313a13f37378012
Ruixuan <ruixuanhou@gmail.com>
2013-11-02 22:04:48 -0400

    Update zigzag.java

--- zigzag.java
+++ zigzag.java
@@ -8,47 +8,32 @@
  * }
  */
 public class Solution {
-    private ArrayList<ArrayList<Integer>> answer;
-    private LinkedList<TreeNode> bfs;
-    private LinkedList<Integer> levels;
-    private void put(TreeNode root, int level){
-        if (null != root){
-            bfs.add(root);
-            levels.add(level);
-        }
-    }
-
-    private void bfs_search(TreeNode root){
-        levels = new LinkedList<Integer>();
-        answer = new ArrayList<ArrayList<Integer>>();
-        bfs = new LinkedList<TreeNode>();
-        ArrayList<Integer> one_level = new ArrayList<Integer>();
-        ArrayList<Integer> one_answer = new ArrayList<Integer>();
-        int pre_level = 0;
-        put(root, 0);
-        
-        while(!bfs.isEmpty()){
-            TreeNode curr = bfs.remove();
-            int curr_level = levels.remove();
-            if (curr_level != pre_level){
-                if (pre_level % 2 != 1)
-                    Collections.reverse(one_answer);
-                answer.add(one_answer);
-                one_answer = new ArrayList<Integer>();
-                pre_level = curr_level;
-            }
-            one_answer.add(curr.val);
-            put(curr.left, curr_level + 1);
-            put(curr.right, curr_level + 1);
-        }
-        if (!one_answer.isEmpty())
-            answer.add(one_answer);
-    }
-
+    
     public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
         // Start typing your Java solution below
         // DO NOT write main() function
-        bfs_search(root);
+        ArrayList<ArrayList<Integer>> answer = new rrayList<ArrayList<Integer>>();
+        if (root == null)
+            return answer;
+        LinkedList<TreeNode> next = new LinkedList<TreeNode>();
+        LinkedList<TreeNode> pre = new LinkedList<TreeNode>();
+        int order = 0;
+        pre.add(root);
+        
+        while(pre.size() != 0){
+            ArrayList<Integer> one_line = new ArrayList<Integer>();
+            for(TreeNode curr:pre){
+                if(curr.left != null)
+                    next.add(curr.left);
+                if(curr.right != null)
+                    next.add(curr.right);
+                one_line.add(curr.val);
+            }
+            if(order == 1)
+                Collections.reverse(one_line);
+            answer.add(one_line);
+            order = 1 - order;
+        }
         return answer;
     }
 }
