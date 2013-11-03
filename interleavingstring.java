7d57b1a9a2fbadc7d85a9f1e594e3eae1a4016fa
Ruixuan <ruixuanhou@gmail.com>
2013-11-03 15:09:51 -0500

    Update interleavingstring.java

--- interleavingstring.java
+++ interleavingstring.java
@@ -7,18 +7,25 @@ public class Solution {
         n = s2.length();
         if(m + n != s3.length())
             return false;
-        boolean dp[][] = new int[m+1][n+1];
+        boolean dp[][] = new boolean[m+1][n+1];
         for(int i = 0; i <= m; i ++)
             for(int j = 0; j <= n; j ++)
                 dp[i][j] = false;
+                
         dp[0][0] = true;
-        for(int i = 1; i <= m; i ++)
-            for(int j = 1; j <= n; j ++){
-                if(s1.charAt(i-1) == s3.charAt(i+j-1))
+        
+        for(int i = 0; i <= m; i ++)
+            for(int j = 0; j <= n; j ++){
+                
+                if(i > 0)
+                  if(s1.charAt(i-1) == s3.charAt(i+j-1))
                     dp[i][j] |= dp[i-1][j];
-                if(s1.charAt(j-1) == s3.charAt(i+j-1))
+                    
+                if(j > 0)
+                 if(s2.charAt(j-1) == s3.charAt(i+j-1))
                     dp[i][j] |= dp[i][j-1];
             }
+            
         return dp[m][n];
     }
 }
