8c33578b674de11ddab06ebf0a95e61393ffc792
Ruixuan <ruixuanhou@gmail.com>
2013-11-03 14:45:00 -0500

    Update valid_number.java

--- valid_number.java
+++ valid_number.java
@@ -1,4 +1,13 @@
 public class Solution {
+    private String remove_front(String s){
+        if (s.length() == 0)
+            return s;
+        if (s.charAt(0) == '-' || s.charAt(0) == '+')
+            return s.substring(1);
+        else 
+            return s;
+    }
+    
     private boolean is_all_digit(String s){
          for( char a:s.toCharArray()){
            if  ( a <'0' || a > '9')
@@ -11,12 +20,16 @@ public class Solution {
         // Start typing your Java solution below
         // DO NOT write main() function
         s = s.trim();
+        s = remove_front(s);
         int index_e;
         index_e = s.indexOf('e');
         if (index_e != -1){
-            if(!is_all_digit(s.substring(index_e + 1)))
+            String power = s.substring(index_e + 1);
+            power = remove_front(power);
+            if(!is_all_digit(power))
                 return false;
-            if(index_e == s.length())
+                
+            if(power.length() == 0)
                 return false;
         }else
             index_e = s.length();
@@ -26,16 +39,23 @@ public class Solution {
         index_dot = s.indexOf('.');
         if (index_dot != -1){
             String front, end;
-            front = s.substring(0, index_dot);
-            end = s.substring(index_dot + 1);
+            
+            front = remove_front(s.substring(0, index_dot));
+            
+            if (index_dot == s.length() - 1)
+                end = "";
+            else
+                end = s.substring(index_dot + 1);
+                
+            if (end.length() != 0)    
+                if (end.charAt(0) == '-')
+                    return false;
             if ( !(is_all_digit(front) && is_all_digit(end)))
                 return false;
             if(front.length() + end.length() == 0)
                 return false;
         }else
             return is_all_digit(s) && s.length() > 0;
-            
         return true;
-       
     }
 }
