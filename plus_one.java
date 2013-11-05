0ddc652501c0e25524c17d2f40a87fd058928c2d
Ruixuan <ruixuanhou@gmail.com>
2013-11-05 11:21:22 -0500

    Update plus_one.java

--- plus_one.java
+++ plus_one.java
@@ -1,17 +1,28 @@
 public class Solution {
+    private void reverse(int[] a){
+        int len = a.length;
+        for(int i = 0; i < len/2; i ++){
+            int tmp = a[i];
+            a[i] = a[len - i - 1];
+            a[len - i - 1] = tmp;
+        }
+    }
     public int[] plusOne(int[] digits) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
+        reverse(digits);
         int len = digits.length;
         int flag = 1;
         // to check if all number equals 9
         for(int val:digits)
             if(val != 9)
                 flag = 0;
+                
         len += flag;
         
         int[] answer = new int[len];
         int inc = 1;
+        
         for(int i = 0; i < digits.length; i ++){
             answer[i] = digits[i] + inc;
             if( answer[i] >= 10){
@@ -22,8 +33,8 @@ public class Solution {
         }
 
         if(inc == 1)
-            answer[len - 1] = 1;
-
+            answer[digits.length] = 1;
+        reverse(answer);
         return answer;
     }
 }
