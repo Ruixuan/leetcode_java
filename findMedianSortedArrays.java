5862a5c0370509be294fbe63871ac9fb5e6b0074
Ruixuan <ruixuanhou@gmail.com>
2013-11-06 16:30:38 -0500

    Update findMedianSortedArrays.java

--- findMedianSortedArrays.java
+++ findMedianSortedArrays.java
@@ -4,7 +4,7 @@ public class Solution {
         // two pointer: i, j and i + j = k
         // 1 3 5 7 9
         // 2 4 6 8 10     
-        int i, j;
+        int i = 0, j = 0;
         int head, tail, mid;
         head = 1;
         tail = A.length;
@@ -21,7 +21,7 @@ public class Solution {
             if ( j < B.length)
                 if(A[i - 1] > B[j])
                     flag = -1;
-            if ( i < A.length)
+            if (i < A.length)
                 if(A[i] < B[i - 1])
                     flag = 1;
             if (flag == 0)
@@ -39,8 +39,8 @@ public class Solution {
         int m = A.length;
         int n = B.length;
         if ( (m + n) % 2 == 1)
-            return find(A, B, (m + n) / 2)
+            return find(A, B, (m + n) / 2);
         else
-            return ((double)(find(A, B, (m + n) / 2) + find(A, B, (m + n) / 2 - 1))) / 2
+            return ((double)(find(A, B, (m + n) / 2) + find(A, B, (m + n) / 2 - 1))) / 2;
     }
 }
