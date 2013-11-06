881f9fc42d9d0850950fd9a15c4cf196c45f7054
Ruixuan <ruixuanhou@gmail.com>
2013-11-05 22:30:53 -0500

    Create count_regions01.py

---
+++ others/count_regions01.py
@@ -0,0 +1,29 @@
+def regions(mat):
+    m = len(mat)
+    n = len(mat[0])
+    count = 0
+    for i in range(m):
+        j = 0
+        while j < n:
+            if mat[i][j] == 0:
+                flag = 1
+                while j < n and mat[i][j] == 0:
+                    if i > 0:
+                        if mat[i - 1][j] == 0:
+                            flag = 0
+                    j += 1
+            count += flag
+            j += 1
+    
+    return count
+    
+# test
+mat = [ [0,0,1,1,1],
+        [0,1,0,0,1],
+        [1,1,1,0,0],
+        [1,0,1,1,1],
+        [1,1,1,1,1]        
+        ]
+    
+print regions(mat)    
+                  
