2d8cf1e8c52a0d74ab48cf2da541c7e70f55eb4a
Ruixuan <hou@wildcat.cs.dartmouth.edu>
2013-11-08 18:50:54 -0500

    another power xn

---
+++ power_xn.java
@@ -0,0 +1,23 @@
+public class Solution{
+    public double pow(double x, int n){
+        int flag = 0;
+        if (n == 0)
+            return 1;
+        if (n < 0){
+            flag = 1;
+            n = -n;
+        }
+        double answer = 1;
+        while(n > 0){
+            if ( n & 1 == 1)
+                answer *= x;
+            x = x * x;
+            n = n << 1;
+
+        }
+        if (flag == 1)
+           return 1.0 / answer;
+        else
+           return answer;
+    }
+}
