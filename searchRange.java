6683a1c559c8d5d04933d8221c445396b408c94d
Ruixuan <ruixuanhou@gmail.com>
2013-11-08 13:02:36 -0500

    Create searchRange.java

---
+++ searchRange.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public int[] searchRange(int[] A, int target) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        
+    }
+}
