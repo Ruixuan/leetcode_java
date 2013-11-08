0c10494b199ddb0ae0a87e1a3f244c58e4d28f62
Ruixuan <hou@wildcat.cs.dartmouth.edu>
2013-11-08 13:22:11 -0500

    w

--- searchInsert.java
+++ searchInsert.java
@@ -2,6 +2,18 @@ public class Solution {
     public int searchInsert(int[] A, int target) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
-        
+        int head, tail, mid;
+        head = 0;
+        tail = A.length - 1;
+        while( head <= tail){
+            mid = head + (tail - head) / 2;
+            if(A[mid] == target)
+                return mid;
+            if (A[mid] < target)
+                head = mid + 1;
+            else
+                head = mid - 1;
+        }
+        return tail + 1;
     }
 }
--- searchRange.java
+++ searchRange.java
@@ -2,6 +2,33 @@ public class Solution {
     public int[] searchRange(int[] A, int target) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
-        
+        int head, tail, mid;
+        int[] answer = new int[]{-1, -1};
+        if (A.length == 0)
+            return answer;
+
+        head = 0;
+        tail = A.length - 1;
+        while(head <= tail){
+            mid = (head + tail) / 2;
+            if (A[mid] <= target)
+                head = mid + 1;
+            else
+                tail = mid - 1;
+        }
+        if (A[tail] != target)
+            return answer;
+        answer[1] = tail;
+        head = 0;
+        tail = A.length - 1;
+        while(head <= tail){
+            mid = (head + tail) / 2;
+            if (A[mid] < target)
+                head = mid + 1;
+            else
+                tail = mid - 1;
+        }
+        answer[0] = head;
+        return answer;
     }
 }
