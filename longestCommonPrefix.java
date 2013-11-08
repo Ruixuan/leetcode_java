8414dfc6a666c36fda37b2455e9c0fc8f4332f3f
Ruixuan <hou@wildcat.cs.dartmouth.edu>
2013-11-08 14:57:58 -0500

    longest common prefix

--- longestCommonPrefix.java
+++ longestCommonPrefix.java
@@ -1,7 +1,20 @@
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
-        // IMPORTANT: Please reset any member data you declared, as
+        // IMPORTANT: Please reset any member data you declared, asa
         // the same Solution instance will be reused for each test case.
-        
+        if (strs.length == 0)
+            return null;
+        len = 0;
+        char curr;
+        int flag = 1;
+        while(flag == 1 && len < strs[0].length()){
+            curr = strs[0].charAt(len);
+            for(String s:strs)
+                if (s.length() <= len || s.charAt(len) != curr)
+                    flag = 0;
+            if (flag == 1)
+                len ++;
+        }
+        return len;
     }
 }
