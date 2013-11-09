5bdc31245ee103fa71b92882a2c98e20bcd01b51
Ruixuan <hou@wildcat.cs.dartmouth.edu>
2013-11-09 17:35:13 -0500

    done merge k

--- mergeksortedlist.java
+++ mergeksortedlist.java
@@ -10,9 +10,33 @@
  * }
  */
 public class Solution {
+    private class MyComparator implements Comparator<ListNode>{
+        @Override
+        public int compare(ListNode a, ListNode b){
+            return a.val - b.val;
+        }
+    }
+
     public ListNode mergeKLists(ArrayList<ListNode> lists) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
-        
+        int n;
+        n = lists.size();
+        PriorityQueue q = new PriorityQueue(n, new MyComparator());
+        for(ListNode one_list: lists){
+            if (null != one_list)
+                    q.offer(one_list);
+        }
+        ListNode answer = new ListNode(0);
+        ListNode pt = answer;
+        while(null != q.peek()){
+            ListNode curr_node = q.poll();
+            pt.next = curr_node;
+            if (curr_node.next != null)
+                q.offer(curr_node.next);
+            pt = pt.next;
+            pt.next = null;
+        }
+        return answer.next;
     }
 }
