1bf82a821950e3d04482df0b55d6f41fbd52054b
Ruixuan <ruixuanhou@gmail.com>
2013-11-08 19:54:54 -0500

    Create addBinary.java

---
+++ addBinary.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public String addBinary(String a, String b) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        
+    }
+}
