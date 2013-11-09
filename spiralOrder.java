eef9646967fdcd1b50ff635c45cff5c078409f70
Ruixuan <ruixuanhou@gmail.com>
2013-11-08 19:34:01 -0500

    Update spiralOrder.java

--- spiralOrder.java
+++ spiralOrder.java
@@ -12,18 +12,18 @@ public class Solution {
         for(int l = 0; l < layers; l ++){
             // print hori 1
             for(int j = l; j < m - l; j ++)
-                answer.append(matrix[l][j]);
+                answer.add(matrix[l][j]);
             // print verti 1
             for(int i = l + 1; i < n - l - 1; i ++)
-                answer.append(matrix[i][m - l - 1]);
+                answer.add(matrix[i][m - l - 1]);
             // print hori 2
-            if (l < m - l - 1)
-                for(int j = m - l - 1; j >= l ; j --)
-                    answer.append(matrix[n - l - 1][j]);
-            // print verti 2
             if (l < n - l - 1)
+                for(int j = m - l - 1; j >= l ; j --)
+                    answer.add(matrix[n - l - 1][j]);
+            // print verti 2
+            if (l < m - l - 1)
                 for(int i =  n - l - 2; i >= l + 1; i --)
-                    answer.append(matrix[i][l]);
+                    answer.add(matrix[i][l]);
         }
         return answer;
     }
