666ceecafd33716fc0ad1d9de15aa3f6593eae3f
Ruixuan <ruixuanhou@gmail.com>
2013-11-08 19:34:52 -0500

    Create spirialmatrixII.java

---
+++ spirialmatrixII.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public int[][] generateMatrix(int n) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        
+    }
+}
