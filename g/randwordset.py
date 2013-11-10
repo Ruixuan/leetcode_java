78e428140de15a9346dd21d2f392cec6c916d1fa
Ruixuan <hou@wildcat.cs.dartmouth.edu>
2013-11-10 15:24:26 -0500

    g still not tested

--- g/randwordset.py
+++ g/randwordset.py
@@ -1,5 +1,4 @@
 /* 
-
 Random set of WORD. 
 
 Criterion : Given a word find out if the word can be broken into smaller word, by removing one alphabet at a time. 
@@ -9,5 +8,24 @@ b) Remove one alphabet at a time but the new word need to be in dictionary.
 For eg : 
 restated -> restate -> estate -> state -> stat -> sat -> at -> a 
 fullfill the criterion. ( single alphabet assume belong to dict) 
-My solution below. I assume it can be done using dynamic programming or trie data structure 
 */
+
+def test_word(s, dic):
+    if len(s) == 0:
+       return True
+
+    if s not in dic:
+        return False
+
+    if result_cache.hasKey(s):
+        return result_cache[s]
+
+    for i in range(len(s)):
+        new_s = s[0:i] + s[i+1:]
+        if test_word(new_s):
+            result_cache[s] = True
+            return True
+
+    result_cache[s] = False
+
+    return False
