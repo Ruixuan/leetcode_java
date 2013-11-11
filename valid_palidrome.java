21e631f2218867ef76996fb03225a633e0a77167
Ruixuan <hou@wildcat.cs.dartmouth.edu>
2013-11-10 19:25:35 -0500

    valid pali

--- valid_palidrome.java
+++ valid_palidrome.java
@@ -2,6 +2,21 @@ public class Solution {
     public boolean isPalindrome(String s) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
+        int head, tail;
+        boolean flag;
+        flag = true;
+        head = 0;
+        tail = s.length() - 1;
+        while(head < tail){
+            while(head < tail && s.charAt(head) == ' ') head ++;
+            while(head < tail && s.charAt(tail) == ' ') tail --;
+            if (head < tail)
+                if (s.charAt(head) != s.charAt(tail)){
+                    flag = false;
+                    break;
+                }
+        }
+        return flag;
         
     }
 }
