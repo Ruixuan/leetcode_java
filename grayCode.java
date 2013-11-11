83de8286f6c5c2d793f122b72de9a07041d30a9c
Ruixuan <ruixuanhou@gmail.com>
2013-11-11 13:50:00 -0500

    Update grayCode.java

--- grayCode.java
+++ grayCode.java
@@ -2,10 +2,12 @@ public class Solution{
     public ArrayList<Integer> grayCode(int n){
         ArrayList<Integer> answer = new ArrayList<Integer>();
         answer.add(0);
-        int m = 1 << n;
+        if (n == 0)
+            return answer;
+        int m = 1 << n ;
         int[] bits = new int[m];
         int pow2 = 0;
-        for(int i = 0; i < m; i ++){
+        for(int i = 0; i < m - 1; i ++){
             if (i + 1 == (1 << pow2)){
                 bits[i] = pow2;
                 pow2 ++;
