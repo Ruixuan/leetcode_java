232b4b6ff2b19f02d1086f6ad5e48470cb10d98d
Ruixuan <ruixuanhou@gmail.com>
2013-11-11 00:44:47 -0500

    Update lru.java

--- lru.java
+++ lru.java
@@ -1,8 +1,8 @@
 public class LRUCache {
 
     private class Node{
-        Node pre, next;
-        int val, key;
+        public Node pre, next;
+        public int val, key;
 
         public Node(int val, int key){
             this.val = val;
@@ -25,28 +25,38 @@ public class LRUCache {
     }
 
     private void add(Node curr){
+        if (capacity == 0)
+            return;
+            
+        if (size == capacity){
+            node_map.remove(tail.key);
+            tail = tail.pre;
+            if (tail != null)
+                tail.next = null;
+            else
+                head = null;
+            size --;
+        }
+        
         if (head == null){
             head = curr;
             tail = curr;
-            node_map.put(curr.key, curr);
         }else{
             curr.next = head;
             head.pre = curr;
             head = curr;
         }
+        node_map.put(curr.key, curr);
         size ++;
-
-        if (size > capacity){
-            node_map.remove(tail.key);
-            tail = tail.pre;
-            tail.next = null;
-            size --;
-        }
+        
     }
 
     private void update(Node curr){
         if (curr.pre == null)
             return;
+        if (curr == tail)
+            tail = curr.pre;
+            
         curr.pre.next = curr.next;
         if (curr.next != null)
             curr.next.pre = curr.pre;
@@ -58,6 +68,8 @@ public class LRUCache {
 
     public int get(int key) {
        Node curr;
+       if (!node_map.containsKey(key))
+          return -1;
        curr = node_map.get(key);
        update(curr);
        return curr.val;
@@ -65,8 +77,8 @@ public class LRUCache {
     
     public void set(int key, int value){
        Node curr;
-       if(!node_map.containskey(key)){
-           add(new node(value, key));
+       if(!node_map.containsKey(key)){
+           add(new Node(value, key));
        }
        curr = node_map.get(key);
        curr.val = value;
