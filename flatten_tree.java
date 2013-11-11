fcbc2c6bf84a3ea03b5269e2c7f996e9cb697efc
Ruixuan <hou@wildcat.cs.dartmouth.edu>
2013-11-10 21:35:50 -0500

    flatten

--- flatten_tree.java
+++ flatten_tree.java
@@ -8,9 +8,27 @@
  * }
  */
 public class Solution {
+    private TreeNode flat(TreeNode root, TreeNode tail){
+        if (root == null)
+            return null;
+
+        TreeNode l, r;
+        r = flat(root.right, root);
+        l = flat(root.left, tail);
+        if (l == null)
+            root.left = tail;
+
+        root.right = null;
+        if(r != null)
+            return r;
+        else 
+            return root;
+
+    }
+
     public void flatten(TreeNode root) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
-        
+        return flat(root, null);
     }
 }
