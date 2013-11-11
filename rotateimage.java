1b6723dfb880c787b4d8a0ea22e0136466ebed89
Ruixuan <ruixuanhou@gmail.com>
2013-11-10 21:14:04 -0500

    Update rotateimage.java

--- rotateimage.java
+++ rotateimage.java
@@ -2,15 +2,16 @@ public class Solution{
     public void rotate(int[][] matrix){
         int n, half;
         n = matrix.length;
-        half = ( n + 1 ) / 2;
-        for(int i = 0; i < half; i ++)
-            for(int j = 0; j <half; j ++){
+      
+        half = ( n ) / 2;
+        for(int i = 0; i < n / 2 ; i ++)
+            for(int j = 0; j < (n + 1) / 2; j ++){
                 int tmp;
-                next = matrix[i][j];
-                matrix[i][j] = matrix[n-j][i];
-                matrix[n-j][i] = matrix[n-i][n-j];
-                matrix[n-i][n-j] =  matrix[j][n - i];
-                matrix[j][n - i] = tmp;
+                tmp = matrix[i][j];
+                matrix[i][j] = matrix[n-j - 1][i];
+                matrix[n-j - 1][i] = matrix[n-i - 1][n-j - 1];
+                matrix[n-i - 1][n-j - 1] =  matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = tmp;
             }
     }
-}
+} 
