d4e9b33d1cf40906336b8f546ae62086b136a4b7
Ruixuan <ruixuanhou@gmail.com>
2013-11-10 22:51:41 -0500

    Create postorderTraversal.java

---
+++ postorderTraversal.java
@@ -0,0 +1,16 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ArrayList<Integer> postorderTraversal(TreeNode root) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        
+    }
+}
