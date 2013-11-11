843b61dcf155ed43be96598cdd01e454a8e32b3c
Ruixuan <hou@wildcat.cs.dartmouth.edu>
2013-11-10 20:35:32 -0500

    anagram rewrite

--- anagrams.java
+++ anagrams.java
@@ -7,24 +7,19 @@ public class Solution {
     public ArrayList<String> anagrams(String[] strs) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
-        HashMap<String, String> sig_map = new HashMap<String, String>();
-        String target = null;
+        HashMap<String, ArrayList<String> > sig_map = new HashMap<String, ArrayList<String>>();
         ArrayList<String> answer = new ArrayList();
         for(String s:strs){
             String sig = signiture(s);
-            if (target == null){
-                if (sig_map.containsKey(sig)){
-                    target = sig;
-                    answer.add(sig_map.get(sig));
-                    answer.add(s);
-                }else{
-                    sig_map.put(sig, s);
-                }
-            }else{
-                // target not null
-                if (target.equals(sig))
-                    answer.add(s);
-            }
+            ArrayList<String> curr;
+            if (!sig_map.containsKey(sig))
+                sig_map.put(sig, new ArrayList<String>());
+            curr = sig_map.get(sig);
+            curr.add(s);
+        }
+
+        for(ArrayList<String> one_list:sig_map){
+            answer.addAll(one_list);
         }
         return answer;
     }
