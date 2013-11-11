1302373475bbc8972be77bcaff948f4ef1a5a8e0
Ruixuan <hou@wildcat.cs.dartmouth.edu>
2013-11-10 23:13:19 -0500

    post order

--- postorderTraversal.java
+++ postorderTraversal.java
@@ -11,6 +11,28 @@ public class Solution {
     public ArrayList<Integer> postorderTraversal(TreeNode root) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
-        
+        TreeNode pre, curr;
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        s.push(null);
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+        pre = new TreeNode(1);
+        curr = root;
+        while(!s.isEmpty()){
+            if (curr != null){
+                if(curr.right == pre){
+                    // right has been traveled
+                    answer.add(curr.val);
+                }else{
+                    s.push(curr);
+                    if(pre != curr.left)
+                        s.push(curr.left);
+                    else
+                        s.push(curr.right);
+                }
+            }
+            pre = curr;
+            curr = s.pop();
+       }
+       return answer;   
     }
 }
