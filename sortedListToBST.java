5c049c8c9ada88cf06fe9172ff48bdfe9ada9943
Ruixuan <hou@tahoe.cs.dartmouth.edu>
2013-11-11 12:04:51 -0500

    linkedList to BST

--- sortedListToBST.java
+++ sortedListToBST.java
@@ -16,9 +16,45 @@
  * }
  */
 public class Solution {
+    private class Result{
+        public TreeNode root;
+        public ListNode tail;
+        public Result(TreeNode root, ListNode tail){
+            this.root = root;
+            this.tail = tail;
+        }
+    }
+    private Result buildTree(ListNode head, int len ){
+        // given head and tail 
+        Result result = new Result(null, head);
+        if (head == null)
+            return result;
+        if (len <= 0)
+            return result;
+        TreeNode left, right;
+        // travel left 
+        Result l = buildTree(head, len  / 2);
+        // create root 
+        result.root = new TreeNode(l.tail.val);
+        result.root.left = l.root;
+        // travel right 
+        Result r = buildTree(l.tail.next, len - len / 2 - 1);
+        result.root.right = r.root;
+        result.tail = r.tail;
+        return result;
+    }
+
     public TreeNode sortedListToBST(ListNode head) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
-        
+        int len;
+        len = 0;
+        ListNode ptr;
+        ptr = head;
+        while( ptr != null){
+            ptr = ptr.next;
+            len ++;
+        }
+        return buildTree(head, n).root;       
     }
 }
