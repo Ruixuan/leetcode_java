8884a8fe2ee560959915ad225447e8e757784cc0
Ruixuan <hou@wildcat.cs.dartmouth.edu>
2013-11-10 20:26:43 -0500

    anagrams done

--- anagrams.java
+++ anagrams.java
@@ -1,7 +1,32 @@
 public class Solution {
+    private String signiture(String s){
+        char[] s_arr = s.toCharArray();
+        Arrays.sort(s_arr);
+        return new String(s_arr);
+    }
     public ArrayList<String> anagrams(String[] strs) {
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
+        HashSet<String, String> sig_map = new HashSet<String, String>();
+        String target = null;
+        ArrayList<String> answer = new ArrayList();
+        for(String s:strs){
+            String sig = signiture(s);
+            if (target == null){
+                if (sig_map.containsKey(sig)){
+                    target = sig;
+                    answer.add(sig_map.get(sig));
+                    answer.add(s);
+                }else{
+                    sig_map.put(sig, s);
+                }
+            }else{
+                // target not null
+                if (target.equals(sig)
+                    answer.add(s);
+            }
+        }
+        return answer;
         
     }
 }
