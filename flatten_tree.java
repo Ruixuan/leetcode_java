e4b9835271712eb41fcea83115036a04a1ed553a
Ruixuan <ruixuanhou@gmail.com>
2013-11-10 21:26:25 -0500

    Create flatten_tree.java

---
+++ flatten_tree.java
@@ -0,0 +1,16 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void flatten(TreeNode root) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        
+    }
+}
