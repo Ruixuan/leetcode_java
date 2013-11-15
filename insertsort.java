f7577c5229753d059e91334e863c2611f6cdc24d
Ruixuan <ruixuanhou@gmail.com>
2013-11-15 04:49:46 -0500

    Create insertsort.java

---
+++ insertsort.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    private void insert(ListNode newNode, ListNode sorted){
+        ListNode curr;
+        curr = sorted;
+        while(curr.next != null || curr.next.val <= newNode){
+            curr = curr.next;
+        }
+        newNode.next = curr.next;
+        curr.next = newNode;
+    }
+    public ListNode insertionSortList(ListNode head) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        ListNode sorted, ptr;
+        sorted = new ListNode(-99999);
+        ptr = head;
+        while(ptr != null){
+            ListNode tmp = ptr.next;
+            insert(ptr, sorted);
+            ptr = tmp;
+        }
+        return sorted.next;
+    }
+}
